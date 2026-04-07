71ee4d8c2c131fe6cbca1bd629417fbbf340486f
Kyle <kylekenrick55@gmail.com>
2026-04-07 23:41:33 +0100

    initialise_board.py: expressions on lines 15 and 27 changed to ternary expressons.

--- flaskr/initialise_board.py
+++ flaskr/initialise_board.py
@@ -1,4 +1,3 @@
-# place_settlement.py
 from flask import (
     Blueprint, g, redirect, render_template, request, url_for
 )
@@ -13,18 +12,19 @@ def place_settlement():
     settlers = get_settlers.get_settlers(db.database_connector)
     resources = get_resources.get_resources(db.database_connector)
     current_settler = [settler for settler in settlers if settlers.victory_points == 0][0]
-    is_last_player = current_settler.id == len(settlers)
+    is_last_settler = True if current_settler.id == len(settlers) else False
     return render_template('initalise_board/place_settlement.html',
                             current_settler_name = current_settler['name'],
-                            is_last_player = is_last_player,
+                            is_last_player = is_last_settler,
                             resources = resources)
 
 @bp.route('/place_city')
 def place_city():
+    update_game_progress.update_game_progress(db.database_connector, "initial city placement")
     settlers = get_settlers.get_settlers(db.database_connector)
     resources = get_resources(db.database_connector)
-    current_settler = [settler for settler in settlers if settlers.victory_points == 1][0]
-    is_last_player = current_settler.id == len(settlers)
+    current_settler = [settler for settler in settlers if settlers.victory_points == 1][0] # TODO
+    is_last_settler = True if current_settler.id == len(settlers) else False
     return render_template('initalise_board/place_city.html', current_settler = current_settler,
-                            is_last_player = is_last_player,
+                            is_last_player = is_last_settler,
                             resources = resources)
