13878e2e4dcd0a6321b081f383ebf53e6f97838c
Kyle <kylekenrick55@gmail.com>
2026-04-08 00:24:48 +0100

    insert_settler_into_settlers_table: function renamed to insert_settler_into_settlers_table.

--- flaskr/helper_modules/insert_settler_into_settlers_table.py
+++ flaskr/helper_modules/insert_settler_into_settlers_table.py
@@ -1,6 +1,4 @@
-#Insert name into settlers table
-
-def insert_player_into_settlers_table(database_connector, username):
+def insert_settler_into_settlers_table(database_connector, username):
   
     with database_connector() as db:
         return db.execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
