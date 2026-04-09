ad1b45cb88634758af0a7eac606cde07a8693b24
Kyle <kylekenrick55@gmail.com>
2026-04-10 00:48:40 +0100

    clear_game_tables.py module added to helper_modules.

---
+++ flaskr/helper_modules/clear_game_tables.py
@@ -0,0 +1,7 @@
+def clear_game_tables(database_connector):
+		with database_connector() as db:
+            db.execute("""DELETE FROM 'settlers';
+            DELETE FROM 'settlements';
+            DELETE FROM 'knights';
+            DELETE FROM 'resources';
+            DELETE FROM 'city_resources_comodities';""")
