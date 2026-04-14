99eb8d192c46771b29a8aa94c21a40e31d65f05a
Kyle <kylekenrick55@gmail.com>
2026-04-15 00:03:24 +0100

    clear_game_tables.py: city_resource_commodities typo fixed.

--- flaskr/helper_modules/clear_game_tables.py
+++ flaskr/helper_modules/clear_game_tables.py
@@ -1,4 +1,4 @@
 def clear_game_tables(database_connector):
     with database_connector() as db:
-      	for game_data_table in [ 'settlers', 'settlements', 'knights', 'resources', 'city_resources_comodities' ]:
+      	for game_data_table in [ 'settlers', 'settlements', 'knights', 'resources', 'city_resources_commodities' ]:
     	    db.execute(f"""DELETE FROM {game_data_table}""")
