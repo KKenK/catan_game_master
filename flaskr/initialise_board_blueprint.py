e6ec754f6bd73e30d398335184e17191099c3a7c
Kyle <kylekenrick55@gmail.com>
2026-04-21 23:46:58 +0100

    initialise_board_blueprint.py: import db removed. Helper module functions updated to reflect the context manager removal.

--- flaskr/initialise_board_blueprint.py
+++ flaskr/initialise_board_blueprint.py
@@ -1,17 +1,15 @@
-# place_settlement.py
 from flask import (
     Blueprint, g, redirect, render_template, request, url_for
 )
 
-from . import db
 from .helper_modules import get_settlers, get_resources, update_game_progress
 bp = Blueprint('initialise_board', __name__, url_prefix='/initialise_board/')
 
 @bp.route('/place_settlement')
 def place_settlement():
-    update_game_progress.update_game_progress(db.DatabaseConnector, "initial settlement placement")
-    settlers = get_settlers.get_settlers(db.DatabaseConnector)
-    resources = get_resources.get_resources(db.DatabaseConnector)
+    update_game_progress.update_game_progress("initial settlement placement")
+    settlers = get_settlers.get_settlers()
+    resources = get_resources.get_resources()
     current_settler = [settler for settler in settlers if settlers.victory_points == 0][0]
     have_all_settlers_placed_a_settlement = True if current_settler.id == len(settlers) else False
     return render_template('initalise_board/place_settlement.html', current_settler_name = current_settler['name'],
@@ -20,9 +18,9 @@ def place_settlement():
 
 @bp.route('/place_city')
 def place_city():
-    update_game_progress.update_game_progress(db.DatabaseConnector, "initial city placement")
-    settlers = get_settlers.get_settlers(db.DatabaseConnector)
-    resources = get_resources(db.DatabaseConnector)
+    update_game_progress.update_game_progress("initial city placement")
+    settlers = get_settlers.get_settlers()
+    resources = get_resources()
     current_settler = [settler for settler in settlers if settlers.victory_points == 1][-1]
     have_all_settlers_placed_a_city = True if current_settler.id == len(settlers) else False
     return render_template('initalise_board/place_city.html', current_settler_name = current_settler['name'],
