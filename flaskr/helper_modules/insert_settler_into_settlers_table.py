73f5805b3febf2441366f6b8b3c9223f126b34c0
Kyle <kylekenrick55@gmail.com>
2026-04-23 23:55:32 +0100

    insert_settler_into_settlers_table.py: database_connection variable removed.

--- flaskr/helper_modules/insert_settler_into_settlers_table.py
+++ flaskr/helper_modules/insert_settler_into_settlers_table.py
@@ -1,7 +1,5 @@
 from .. import db
 
 def insert_settler_into_settlers_table( username):
-  
-    database_connection = db.get_db()
     
-    return db.execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
+    return db.get_db().execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
