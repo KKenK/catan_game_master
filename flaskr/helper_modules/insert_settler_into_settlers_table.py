803ab36dd36c1156d03f2d04fe794de35b884227
Kyle <kylekenrick55@gmail.com>
2026-04-28 00:34:21 +0100

    insert_into_settlers_table.py: database_connection variable reintroduced and commit() called on it to commit the changes to the database.

--- flaskr/helper_modules/insert_settler_into_settlers_table.py
+++ flaskr/helper_modules/insert_settler_into_settlers_table.py
@@ -1,5 +1,11 @@
 from .. import db
 
-def insert_settler_into_settlers_table( username):
+def insert_settler_into_settlers_table(username):
     
-    return db.get_db().execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
+    database_connection =  db.get_db()
+
+    database_connection.execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
+
+    database_connection.commit()
+
+    return
