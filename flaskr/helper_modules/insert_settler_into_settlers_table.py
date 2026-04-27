206f97842226ae102fccc8ac820137f5365ddeae
Kyle <kylekenrick55@gmail.com>
2026-04-28 00:36:05 +0100

    insert_into_settlers_table.py: settler_id from the username insertion stored in the settler_id.

--- flaskr/helper_modules/insert_settler_into_settlers_table.py
+++ flaskr/helper_modules/insert_settler_into_settlers_table.py
@@ -4,8 +4,8 @@ def insert_settler_into_settlers_table(username):
     
     database_connection =  db.get_db()
 
-    database_connection.execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
+    settler_id = database_connection.execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
 
     database_connection.commit()
 
-    return
+    return settler_id
