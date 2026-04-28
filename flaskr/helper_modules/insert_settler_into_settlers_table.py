053a12bdacb1869af4bf7e3ed67dbc50c962e9d9
Kyle <kylekenrick55@gmail.com>
2026-04-29 00:22:38 +0100

    insert_settler_into_settlers_table.py: calculate_row_id function correctly prefixed.

--- flaskr/helper_modules/insert_settler_into_settlers_table.py
+++ flaskr/helper_modules/insert_settler_into_settlers_table.py
@@ -1,10 +1,13 @@
 from .. import db
+from . import calculate_row_id
 
 def insert_settler_into_settlers_table(username):
     
     database_connection =  db.get_db()
 
-    settler_id = database_connection.execute("""INSERT INTO 'settlers' (username) VALUES (?) RETURNING id""", (username,)).fetchone()[0]
+    settler_id = calculate_row_id.calculate_row_id("settlers")
+
+    database_connection.execute("""INSERT INTO 'settlers' (id, username) VALUES (?, ?)""", (settler_id, username,))
 
     database_connection.commit()
 
