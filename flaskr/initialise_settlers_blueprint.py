28de42994fe7ba424d15525e2c915fef001ddcb1
Kyle <kylekenrick55@gmail.com>
2026-04-30 01:27:44 +0100

    initialise_settlers_blueprint.py: maximum/minimum_player_reached renamed to ..settler_reached.

--- flaskr/initialise_settlers_blueprint.py
+++ flaskr/initialise_settlers_blueprint.py
@@ -9,7 +9,6 @@ bp = Blueprint('initialise_settlers',__name__, url_prefix='/initialise_settlers/
 def select_number_of_players():
     return render_template('select_number_of_players.html')
 
-
 @bp.route('/register_settler', methods=['GET','POST'])
 def register_settlers():
  
@@ -20,12 +19,13 @@ def register_settlers():
     
     next_settler_id = calculate_row_id.calculate_row_id('settlers')
     
-    minimum_players_required = False
-    maximum_players_reached = False
+    minimum_settlers_required = False
+    maximum_settlers_reached = False
 
     if next_settler_id >= 1:
         minimum_players_required = True
     if next_settler_id >= 5:
         maximum_players_reached = True
 
-    return render_template('register_settler.html', next_settler_number = next_settler_id + 1, maximum_settler_reached = maximum_players_reached, minimum_players_required = minimum_players_required)
+
+    return render_template('register_settler.html', next_settler_number = next_settler_id + 1, maximum_settler_reached = maximum_settlers_reached, minimum_settlers_required = minimum_settlers_required)
