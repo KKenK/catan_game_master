7aaf7949b477903517be02fed5935ef05b578c59
Kyle <kylekenrick55@gmail.com>
2026-05-03 00:37:07 +0100

    update_game_progress.py: commit() added to save changes to database.

--- flaskr/helper_modules/update_game_progress.py
+++ flaskr/helper_modules/update_game_progress.py
@@ -5,3 +5,5 @@ def update_game_progress(game_status):
     database_connection = db.get_db()
     
     database_connection.execute("""UPDATE INTO 'game_progress' (progress) VALUES (?)""", (game_status,))
+
+    database_connection.commit()
