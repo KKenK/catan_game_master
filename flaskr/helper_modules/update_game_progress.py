d39e8ce9db8f5e82a9a4088576a1b39acc318264
Kyle <kylekenrick55@gmail.com>
2026-05-03 00:57:16 +0100

    update_game_progress.py: UPDATE query corrected by replacing VALUE with =.

--- flaskr/helper_modules/update_game_progress.py
+++ flaskr/helper_modules/update_game_progress.py
@@ -4,6 +4,6 @@ def update_game_progress(game_status):
     
     database_connection = db.get_db()
     
-    database_connection.execute("""UPDATE 'game_progress' SET (progress) VALUES (?)""", (game_status,))
+    database_connection.execute("""UPDATE 'game_progress' SET (progress) = (?)""", (game_status,))
 
     database_connection.commit()
