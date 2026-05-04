355dfd5c136a72923acefd6f4dd04f5adf64aa3e
Kyle <kylekenrick55@gmail.com>
2026-05-04 22:47:21 +0100

    resources.py helper module committed.

---
+++ flaskr/helper_modules/resources.py
@@ -0,0 +1,5 @@
+base_resources = [(0, "wood"),(1, "brick"),(2, "sheep"),(3, "wheat"),(4, "ore")]
+
+city_resources = [(0, "wood"),(1, "brick"),(2, "sheep"),(3, "wheat"),(4, "ore")]
+
+city_resources_comodities = [(0, "paper"),(1, "brick"),(2, "cloth"),(3, "wheat"),(4, "coin")]
