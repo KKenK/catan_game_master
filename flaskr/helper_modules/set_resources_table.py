b1b1b0c1b9e0ce9694455755190d151b5cdbed98
Kyle <kylekenrick55@gmail.com>
2026-05-04 23:15:20 +0100

    set_resources_table.py: set_resource_commodities_table function added.

--- flaskr/helper_modules/set_resources_table.py
+++ flaskr/helper_modules/set_resources_table.py
@@ -9,4 +9,13 @@ def set_resources_table(resource_tuple_list):
     database_connection.commit()
     
     return 
-  
+
+def set_resources_commodities_table(resource_tuple_list):
+    
+    database_connection = db.get_db()
+    
+    database_connection.executemany("""INSERT INTO 'city_resources_commodities ' (id, name) VALUES (?, ?)""", (resource_tuple_list))
+
+    database_connection.commit()
+    
+    return 
