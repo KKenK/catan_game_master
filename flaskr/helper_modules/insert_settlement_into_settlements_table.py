88a90ebca027ea57754ed19fb934611d08bd0438
Kyle <kylekenrick55@gmail.com>
2026-05-08 00:51:38 +0100

    insert_settlement_into_settlements_table.py helper module commited.

---
+++ flaskr/helper_modules/insert_settlement_into_settlements_table.py
@@ -0,0 +1,20 @@
+from .. import db
+
+def insert_settlement_into_settlements_table(settlement):
+
+    database_connection = db.get_db()
+
+    database_connection.execute("""INSERT INTO 'settlements' (settler_id, 
+                                resource_1, roll_1,
+                                resource_2, roll_2,
+                                resource_3, roll_3,
+                                is_city)
+                                VALUES (?,?,?,?,?,?,?,?)""", (settlement['settler_id'],
+                                settlement['resource_1'], settlement['roll_1'],
+                                settlement['resource_2'], settlement['roll_2'],
+                                settlement['resource_3'], settlement['roll_3'],
+                                settlement['is_city']))
+    
+    database_connection.commit()
+
+    return
