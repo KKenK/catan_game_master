51c43cd971f0192363a205ced44355a026ce9bc6
Kyle <kylekenrick55@gmail.com>
2026-05-10 20:42:08 +0100

    insert_settlement_into_settlements_table.py: calculate_row_id imported and called to calculate the settlement id.

--- flaskr/helper_modules/insert_settlement_into_settlements_table.py
+++ flaskr/helper_modules/insert_settlement_into_settlements_table.py
@@ -1,15 +1,18 @@
 from .. import db
+from . import calculate_row_id 
 
 def insert_settlement_into_settlements_table(settlement):
 
+    settlement_id = calculate_row_id.calculate_row_id("settlements")
+
     database_connection = db.get_db()
 
-    database_connection.execute("""INSERT INTO 'settlements' (settler_id, 
+    database_connection.execute("""INSERT INTO 'settlements' (id, settler_id, 
                                 resource_1, roll_1,
                                 resource_2, roll_2,
                                 resource_3, roll_3,
                                 is_city)
-                                VALUES (?,?,?,?,?,?,?,?)""", (settlement['settler_id'],
+                                VALUES (?,?,?,?,?,?,?,?,?)""", (settlement_id, settlement['settler_id'],
                                 settlement['resource_1'], settlement['roll_1'],
                                 settlement['resource_2'], settlement['roll_2'],
                                 settlement['resource_3'], settlement['roll_3'],
