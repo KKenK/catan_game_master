81d8afd81ea135dcd3ffa758bf18c46d5b3d4585
Kyle <kylekenrick55@gmail.com>
2026-05-10 20:45:50 +0100

    initialise_board_blueprint.py: place_settlement route logic copied over to place_city route.

--- flaskr/initialise_board_blueprint.py
+++ flaskr/initialise_board_blueprint.py
@@ -44,13 +44,36 @@ def place_settlement():
                             have_all_settlers_placed_a_settlement = have_all_settlers_placed_a_settlement,
                             resources = resources)
 
-@bp.route('/place_city')
+@bp.route('/place_city', methods =['GET', 'POST'])
 def place_city():
-    update_game_progress.update_game_progress("initial city placement")
+
+    update_game_progress.update_game_progress("initial settlement placement")
+
     settlers = get_settlers.get_settlers()
+
+    settlers_with_one_victory_points = [settler for settler in settlers if settler['victory_points'] == 1]   
+
+    if request.method == 'POST':
+
+        current_settler = settlers_with_one_victory_points.pop()
+
+        insert_settlement_into_settlements_table.insert_settlement_into_settlements_table({'settler_id': current_settler['id'],
+                'resource_1': request.form['resource_1'], 'roll_1': request.form['roll_1'],
+                'resource_2': request.form['resource_2'], 'roll_2': request.form['roll_2'],
+                'resource_3': request.form['resource_3'], 'roll_3': request.form['roll_3'],
+                'is_city': True})
+        
+        increment_victory_points.increment_victory_points(current_settler['id'], increment_value = 2)
+    
+    if not settlers_with_one_victory_points:
+        have_all_settlers_placed_a_city = True
+        current_settler = {'username' : 'All settlements places!',}
+    else:
+        current_settler = settlers_with_one_victory_points[-1]
+        have_all_settlers_placed_a_city = False
+    
     resources = get_resources.get_resources()
-    current_settler = [settler for settler in settlers if settler['victory_points'] == 1][-1]
-    have_all_settlers_placed_a_city = True if current_settler['id'] == len(settlers) else False
+    
     return render_template('initialise_board/place_city.html', current_settler_name = current_settler['username'],
                             have_all_settlers_placed_a_city = have_all_settlers_placed_a_city,
                             resources = resources)
