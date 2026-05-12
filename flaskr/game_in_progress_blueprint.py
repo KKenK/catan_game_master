6225cd49433c9ca31711d850fa92d0dbdf5bce71
Kyle <kylekenrick55@gmail.com>
2026-05-13 00:08:54 +0100

    game_in_progress_blueprint.py first commit.

---
+++ flaskr/game_in_progress_blueprint.py
@@ -0,0 +1,21 @@
+from flask import (
+    Blueprint, g, redirect, render_template, request, session, url_for
+)
+from .helper_modules import get_settlers, get_knights, get_settlements, update_game_progress
+
+bp = Blueprint('game_in_progress',__name__, url_prefix='/game')
+
+@bp.route('/')
+def game():
+    
+    if update_game_progress.get_game_progress() != "game_in_progress":
+            update_game_progress.update_settler_turn(1)
+            update_game_progress.update_game_progress("game_in_progress")
+
+    settlers = get_settlers.get_settlers()
+
+    knights = get_knights.get_knights()
+        
+    settlements = get_settlements.get_settlements()
+
+    return render_template('game_page.html')
