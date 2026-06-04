f283b46201030f60ada89bdab63db8844cf94883
Kyle <kylekenrick55@gmail.com>
2026-06-04 23:56:42 +0100

    update_settler_turn.py: is_settler_two arguement added to statement.

--- flaskr/helper_modules/update_settler_turn.py
+++ flaskr/helper_modules/update_settler_turn.py
@@ -1,9 +1,9 @@
 from .. import db
 
-def update_settler_turn(settler_turn):
+def update_settler_turn(settler_turn, is_settler_two):
     
     database_connection = db.get_db()
     
-    database_connection.execute("""UPDATE 'game_progress' SET (settler_turn) = (?)""", (settler_turn,))
+    database_connection.execute("""UPDATE 'game_progress' SET (settler_turn, is_settler_two) = (?,?)""", (settler_turn, is_settler_two))
 
     database_connection.commit()
