ade8efdad81cd3edd42a2121365e72df69ad7c42
Kyle <kylekenrick55@gmail.com>
2026-06-14 02:50:54 +0100

    deactivate_knight.py: first commit.

---
+++ flaskr/helper_modules/deactivate_knight.py
@@ -0,0 +1,11 @@
+from .. import db
+
+def activate_knight(knight_id):
+
+    database_connection = db.get_db()
+    
+    database_connection.execute("""UPDATE knights SET is_active = 0 where id = ?""", (knight_id,))
+
+    database_connection.commit()
+
+    return
