1c11103e85ed57b8f9e52611ff91cfab7de58ce9
Kyle <kylekenrick55@gmail.com>
2026-07-03 00:16:43 +0100

    insert_settler_into_settlers_that_contributed_least_to_catans_defence_table.py: tuple inserted into query

--- flaskr/helper_modules/insert_settler_into_settlers_that_contributed_least_to_catans_defence_table.py
+++ flaskr/helper_modules/insert_settler_into_settlers_that_contributed_least_to_catans_defence_table.py
@@ -6,7 +6,7 @@ def insert_settler_into_settlers_that_contributed_least_to_catans_defence_table(
 
     for settler_id in settler_ids:
     
-        database_connection.execute("""INSERT INTO 'settlers_that_contributed_least_to_catans_defence' (id) VALUES (?)""", (settler_id))
+        database_connection.execute("""INSERT INTO 'settlers_that_contributed_least_to_catans_defence' (id) VALUES (?)""", (settler_id,))
 
     database_connection.commit()
 
