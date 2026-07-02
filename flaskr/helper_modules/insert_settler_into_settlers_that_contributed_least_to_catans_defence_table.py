bc54ce5fff9d443803d205289a44887a10062610
Kyle <kylekenrick55@gmail.com>
2026-07-02 23:55:36 +0100

    insert_settler_into_settlers_that_contributed_least_to_catans_defence_table.py: first commit.

---
+++ flaskr/helper_modules/insert_settler_into_settlers_that_contributed_least_to_catans_defence_table.py
@@ -0,0 +1,13 @@
+from .. import db
+
+def insert_settler_into_settlers_that_contributed_least_to_catans_defence_table(settler_ids):
+    
+    database_connection =  db.get_db()
+
+    for settler_id in settler_ids:
+    
+        database_connection.execute("""INSERT INTO 'settlers_that_contributed_least_to_catans_defence' (id) VALUES (?)""", (settler_id))
+
+    database_connection.commit()
+
+    return
