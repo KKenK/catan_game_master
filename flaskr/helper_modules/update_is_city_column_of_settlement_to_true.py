8b313663b3ff8b043870ade6e0d55ef0cf12135d
Kyle <kylekenrick55@gmail.com>
2026-07-04 18:36:13 +0100

    update_is_city_column_of_settlement_to_true.py: brackets around value removed.

--- flaskr/helper_modules/update_is_city_column_of_settlement_to_true.py
+++ flaskr/helper_modules/update_is_city_column_of_settlement_to_true.py
@@ -4,7 +4,7 @@ def update_is_city_column_of_settlement_to_true(settlement_id):
     
     database_connection = db.get_db()
     
-    database_connection.execute("""UPDATE 'settlements' SET (is_city) = (1) WHERE id = (?) """, (settlement_id,))
+    database_connection.execute("""UPDATE 'settlements' SET (is_city) = 1 WHERE id = (?) """, (settlement_id,))
 
     database_connection.commit()
     
