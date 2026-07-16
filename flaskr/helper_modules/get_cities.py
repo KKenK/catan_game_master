ddab60a17e49dbd891a5e106f55e7dafc94c1b2b
Kyle <kylekenrick55@gmail.com>
2026-07-17 00:23:16 +0100

    get_cities.py: get_cities_with_resource_name function added.

--- flaskr/helper_modules/get_cities.py
+++ flaskr/helper_modules/get_cities.py
@@ -5,3 +5,21 @@ def get_cities():
     database_connection = db.get_db()
     
     return database_connection.execute("""SELECT * FROM settlements WHERE is_city = 1""").fetchall()
+
+def get_cities_with_resource_name():
+    
+    database_connection = db.get_db()
+    
+    return database_connection.execute("""SELECT settlements.id,
+    settler_id,
+    resources_1.name AS resource_1_text,
+    roll_1,
+    resources_2.name AS resource_2_text,
+    roll_2,
+    resources_3.name AS resource_3_text,
+    roll_3,
+    is_city FROM settlements  
+    LEFT JOIN resources AS resources_1 ON settlements.resource_1 = resources_1.id
+    LEFT JOIN resources AS resources_2 ON settlements.resource_2 = resources_2.id
+    LEFT JOIN resources AS resources_3 ON settlements.resource_3 = resources_3.id
+    WHERE is_city = 1""").fetchall()
