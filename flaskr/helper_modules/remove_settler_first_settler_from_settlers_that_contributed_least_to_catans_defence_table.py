898553d2e64e9c25b308ed37722fe2a13403becd
Kyle <kylekenrick55@gmail.com>
2026-07-16 23:47:36 +0100

    remove_settler_first_settler_from_settlers_that_contributed_least_to_catans_defence_table.py: deleted.

--- flaskr/helper_modules/remove_settler_first_settler_from_settlers_that_contributed_least_to_catans_defence_table.py
+++
@@ -1,9 +0,0 @@
-from .. import db
-
-def remove_settler_from_settlers_that_contributed_least_to_catans_defence_table(settler_id):
-    
-    database_connection = db.get_db()
-    
-    database_connection.execute("""DELETE FROM 'settlers_that_contributed_least_to_catans_defence' WHERE id = (?)""", (settler_id,))
-
-    database_connection.commit()
