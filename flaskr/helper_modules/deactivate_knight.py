54eaaa8783deafaf58b93d20990523ff875847f4
Kyle <kylekenrick55@gmail.com>
2026-07-24 00:15:33 +0100

    deactivate_knight.py: deactivate all knights function added.

--- flaskr/helper_modules/deactivate_knight.py
+++ flaskr/helper_modules/deactivate_knight.py
@@ -9,3 +9,13 @@ def deactivate_knight(knight_id):
     database_connection.commit()
 
     return
+
+def deactivate_all_knights():
+
+    database_connection = db.get_db()
+    
+    database_connection.execute("""UPDATE knights SET is_active = 0""")
+
+    database_connection.commit()
+
+    return
