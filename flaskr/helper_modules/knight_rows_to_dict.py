523f73a525b3e32774d80d1d4e656e1582e695e0
Kyle <kylekenrick55@gmail.com>
2026-07-28 23:43:21 +0100

    knight_rows_to_dict.py: first commit.

---
+++ flaskr/helper_modules/knight_rows_to_dict.py
@@ -0,0 +1,9 @@
+def knight_rows_to_dict(knight_rows):
+
+    knight_dict = {knight_row['id'] : {'settler_id' : knight_row['settler_id'],
+                                       'level': knight_row['level'],
+                                       'is_active' : knight_row['is_active']
+                                       } for knight_row in knight_rows}
+
+    return knight_rows_to_dict
+
