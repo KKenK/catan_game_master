eb30c4c5ace8dbe568039137597c8e889b41bcca
Kyle <kylekenrick55@gmail.com>
2026-07-29 00:12:53 +0100

    knight_rows_to_dict.py: is_promotable key added.

--- flaskr/helper_modules/knight_rows_to_dict.py
+++ flaskr/helper_modules/knight_rows_to_dict.py
@@ -2,8 +2,8 @@ def knight_rows_to_dict(knight_rows):
 
     knight_dict = {knight_row['id'] : {'settler_id' : knight_row['settler_id'],
                                        'level': knight_row['level'],
-                                       'is_active' : knight_row['is_active']
-                                       } for knight_row in knight_rows}
+                                       'is_active' : knight_row['is_active'],
+                                       'is_promotable' : False} for knight_row in knight_rows}
 
     return knight_dict
 
