dc7096cf675a792e989ba4902788831446b8f3fa
Kyle <kylekenrick55@gmail.com>
2026-07-31 00:40:50 +0100

    clear_game_tables.py: missing comma added to table list.

--- flaskr/helper_modules/clear_game_tables.py
+++ flaskr/helper_modules/clear_game_tables.py
@@ -4,7 +4,7 @@ def clear_game_tables():
   	
     database_connection = db.get_db()
     
-    for game_data_table in ['game_progress' 'settlers', 'settlements', 'knights', 'settlers_that_contributed_least_to_catans_defence','resources', 'city_resources_commodities']:
+    for game_data_table in ['game_progress', 'settlers', 'settlements', 'knights', 'settlers_that_contributed_least_to_catans_defence','resources', 'city_resources_commodities']:
         database_connection.execute(f"""DELETE FROM {game_data_table}""")
 
     database_connection.commit()
