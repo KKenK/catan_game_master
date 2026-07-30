06c572f83eb7c82bfd8802f6af77e40e5c554b20
Kyle <kylekenrick55@gmail.com>
2026-07-31 00:46:31 +0100

    schema.sql: INSERT INTO game_progress removed.

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -4,7 +4,6 @@ CREATE TABLE game_progress (
     is_settler_two INTEGER DEFAULT 0 NOT NULL,
     barbarians_distance_from_catan INTEGER DEFAULT 7 NOT NULL
 );
-INSERT INTO 'game_progress' (progress) VALUES ('uninitialised');
 
 CREATE TABLE settlers (
     id INTEGER PRIMARY KEY,
