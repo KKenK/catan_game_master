b150728d5fa245a728c7a3fbe20917c1a6ba144f
Kyle <kylekenrick55@gmail.com>
2026-08-04 23:25:14 +0100

    populate_dice_roll.py: first commit.

---
+++ flaskr/helper_modules/populate_dice_roll.py
@@ -0,0 +1,11 @@
+from .. import db
+
+def insert_into_dice_roll(red, white, event):
+
+    database_connection =  db.get_db()
+
+    database_connection.execute("""INSERT INTO 'dice_roll' (red, white, event) VALUES (?, ?, ?)""", (red, white, event))
+
+    database_connection.commit()
+
+    return
