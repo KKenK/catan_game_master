570171c979091899f2a6534b170ab7f96ac95456
Kyle <kylekenrick55@gmail.com>
2026-08-18 00:56:30 +0100

    knight.py: first commit.

---
+++ flaskr/helper_classes/knight.py
@@ -0,0 +1,9 @@
+class Knight():
+    def __init__(self, knight_row):
+        self.id = knight_row['id']
+        self.settler_id = knight_row['settler_id']
+        self.level = knight_row['level']
+        self.is_active = knight_row['is_active']
+
+
+
