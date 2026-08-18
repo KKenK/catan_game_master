fd7be16b9241548526b4508198de551f868d5a7c
Kyle <kylekenrick55@gmail.com>
2026-08-19 00:09:08 +0100

    army.py: indexing removed.

--- flaskr/helper_classes/army.py
+++ flaskr/helper_classes/army.py
@@ -2,7 +2,7 @@ class Army():
     def __init__(self, knights, settler_id):
         self.settler_id = settler_id
         self.knights = knights
-        self.army_strength = sum([knight['level'] for knight in knights if knight['is_active']])
+        self.army_strength = sum([knight.level for knight in knights if knight.is_active])
 
 
 
