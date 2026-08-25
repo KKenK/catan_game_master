bb3042bca271525891ccad3fdb6db06d4fc6a7a6
Kyle <kylekenrick55@gmail.com>
2026-08-26 00:02:37 +0100

    settler.py: id attribute added and print_settler_dict method added.

--- flaskr/helper_classes/settler.py
+++ flaskr/helper_classes/settler.py
@@ -1,6 +1,6 @@
 class Settler():
     def __init__(self, settler_row):
-            
+        self.id = settler_row['id']
         self.username = settler_row['username']
         self.victory_point_card = settler_row['victory_point_card'] 
         self.longest_road  = settler_row['longest_road']
@@ -12,3 +12,6 @@ class Settler():
         longest_road_victory_points = 2 if self.has_longest_road else 0
   
         return self.victory_point_card + self.defender_of_catan + longest_road_victory_points + settler_portfolio_worth
+
+    def print_settler_dict(self):
+        print(self.__dict__)
