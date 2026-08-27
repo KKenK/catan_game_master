1a522c52ab27f1cc87a8b4ba212e7d9bfce83850
Kyle <kylekenrick55@gmail.com>
2026-08-27 23:50:54 +0100

    increment_victory_point_cards.py: altered to increment victory_point_cards instead of victory_points.

--- flaskr/helper_modules/increment_victory_point_cards.py
+++ flaskr/helper_modules/increment_victory_point_cards.py
@@ -1,10 +1,10 @@
 from .. import db
 
-def increment_victory_points(settler_id, increment_value = 1):
+def increment_victory_points_cards(settler_id):
 
     database_connection = db.get_db()
 
-    database_connection.execute("""UPDATE settlers SET victory_points = victory_points + (?) WHERE id = (?)""", (increment_value, settler_id,))
+    database_connection.execute("""UPDATE settlers SET victory_point_cards = victory_point_cards + 1 WHERE id = (?)""", (settler_id,))
 
     database_connection.commit()
 
