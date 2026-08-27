5a21981ed5e7ccbc0e3a26708e360051e01ab1a9
Kyle <kylekenrick55@gmail.com>
2026-08-27 23:21:11 +0100

    settlement.py: hexagon imported and incorporated into the settlement class so rolls and resources can be looped over in the collect resources route.

--- flaskr/helper_classes/settlement.py
+++ flaskr/helper_classes/settlement.py
@@ -1,13 +1,12 @@
+from .hexagon import Hexagon
+
 class Settlement():
 
     def __init__(self, settlement_row):
 
         self.id = settlement_row['id']
         self.settler_id = settlement_row['settler_id']
-        self.resource_1 = settlement_row['resource_1']
-        self.roll_1 = settlement_row['roll_1']
-        self.resource_2 = settlement_row['resource_2']
-        self.roll_2 = settlement_row['roll_2']
-        self.resource_3 = settlement_row['resource_3']
-        self.roll_3 = settlement_row['roll_3']
+        self.resource_hexes = [Hexagon(roll = settlement_row['roll_1'], resource = settlement_row['resource_1']),
+                        Hexagon(roll = settlement_row['roll_2'], resource = settlement_row['resource_2']),
+                        Hexagon(roll = settlement_row['roll_3'], resource = settlement_row['resource_3'])]
         self.is_city = settlement_row['is_city']
