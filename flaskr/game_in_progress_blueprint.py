4cff7adddab88ac3c9754def26adac1d2eb78d2e
Kyle <kylekenrick55@gmail.com>
2026-09-06 03:30:09 +0100

    game_in_progress_blueprint.py: classes implemented into the revise_longest_road route and only the settler whose turn it is currently retrieved from the database.

--- flaskr/game_in_progress_blueprint.py
+++ flaskr/game_in_progress_blueprint.py
@@ -353,9 +353,9 @@ def revise_longest_road():
 
         settler_turn_id = get_settler_turn.get()['settler_turn']
 
-        current_settler = [settler for settler in get_settlers.get_settlers() if settler['id'] == settler_turn_id][0]
+        current_settler = Settler(get_settlers.get_settler_via_id(settler_turn_id))
 
-        return render_template('revise_longest_road.html', current_settler = current_settler, current_settler_longest_road = current_settler['longest_road'])
+        return render_template('revise_longest_road.html', current_settler = current_settler, current_settler_longest_road = current_settler.longest_road)
 
 @bp.route('update_longest_road', methods = ['POST'])
 def update_longest_road():
