8e7cd7fbc93720f25273bfd1ac513470a5444962
Kyle <kylekenrick55@gmail.com>
2026-09-06 03:04:56 +0100

    game_in_progress_blueprint.py: build_settlement refactored so only the settler whose turn it is currently is retrieved from the database and instantiated.

--- flaskr/game_in_progress_blueprint.py
+++ flaskr/game_in_progress_blueprint.py
@@ -302,11 +302,11 @@ def build_settlement():
     
     settler_turn_id = get_settler_turn.get()['settler_turn']  
     
-    settlers = row_objects_to_classes.row_objects_to_classes(Settler, get_settlers.get_settlers())
+    settler_to_place_settlement_name = Settler(get_settlers.get_settler_via_id(settler_turn_id))
     
     resources = get_resources.get_resources()  
 
-    return render_template('place_settlement.html', settler_to_place_settlement_name = settlers[settler_turn_id].username,
+    return render_template('place_settlement.html', settler_to_place_settlement_name = settler_to_place_settlement_name.username,
                         have_all_settlers_placed_a_settlement = False,
                         resources = resources)    
 
